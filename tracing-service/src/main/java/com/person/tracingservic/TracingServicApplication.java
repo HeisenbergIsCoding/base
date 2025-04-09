d6e2352d42a0e4c9f812ba6850d1922a772bddc0
Ziv Stark <zivchan.tw@gmail.com>
2025-04-09 12:54:37 +0800

    first commit

---
+++ tracing-service/src/main/java/com/person/tracingservic/TracingServicApplication.java
@@ -0,0 +1,13 @@
+package com.person.tracingservic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingServicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracingServicApplication.class, args);
+    }
+
+}
---
+++ tracing-service/src/test/java/com/person/tracingservic/TracingServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person.tracingservic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingServicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
