f2a59929b5ba8083fcc42a67836ccc3b78c47d64
Ziv Stark <zivchan.tw@gmail.com>
2025-04-18 12:56:20 +0800

    feat: 加入 api-gateway,service-registry,tracing-service的程式

--- tracing-service/src/main/java/com/person/tracingservic/TracingServicApplication.java
+++ tracing-service/src/main/java/com/person/tracingservic/TracingServicApplication.java
@@ -2,8 +2,10 @@ package com.person.tracingservic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class TracingServicApplication {
 
     public static void main(String[] args) {
